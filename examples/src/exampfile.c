91de66e9a7b46632585b65790d8d6f9c5221126e
1Hyena <hyena@hyena.net.ee>
2026-01-25 01:45:02 +0200

    added amp file loading example

---
+++ examples/src/exampfile.c
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+#include "../../amp.h"
+
+
+static struct blob_type {
+    char *data;
+    size_t size;
+} load_file(const char *fname);
+
+
+int main(int argc, char **argv) {
+    const char *error_message = nullptr;
+
+    if (argc < 2) {
+        error_message = "this program requires a file name argument\n";
+        write(2, error_message, strlen(error_message));
+        return EXIT_FAILURE;
+    }
+
+    auto input_file = load_file(argv[1]);
+    char *input_data = input_file.data;
+    size_t input_size = input_file.size;
+
+    if (!input_data) {
+        return EXIT_FAILURE;
+    }
+
+    uint32_t w, h;
+    size_t canvas_size = amp_parse_size(input_data, input_size, &w, &h);
+    uint8_t *canvas_data = nullptr;
+
+    if (canvas_size) {
+        canvas_data = malloc(canvas_size);
+
+        if (canvas_data) {
+            struct amp_type amp;
+
+            if (amp_init(&amp, w, h, canvas_data, canvas_size) <= canvas_size) {
+                if (amp_deserialize(&amp, input_data, input_size)) {
+                    amp_to_ans(&amp, nullptr, 0);
+                    amp_stdout("\n", 1);
+                }
+                else error_message = "amp_deserialize: parse error\n";
+            }
+            else error_message = "amp_init: not enough memory provided\n";
+        }
+        else error_message = "malloc: allocation failed\n";
+    }
+    else error_message = "amp_parse_size: parse error\n";
+
+    free(input_data);
+    free(canvas_data);
+
+    if (error_message) {
+        write(2, error_message, strlen(error_message));
+
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+static struct blob_type load_file(const char *fname) {
+    static constexpr size_t INITIAL_BUFFER_CAPACITY = 512;
+    size_t data_size = 0, data_capacity = INITIAL_BUFFER_CAPACITY;
+    char *data = nullptr;
+    FILE *fp = fopen(fname, "r");
+
+    if (fp == nullptr) {
+        const char *error_message = "fopen: error opening file\n";
+        write(2, error_message, strlen(error_message));
+
+        return (struct blob_type) {};
+    }
+
+    for (;;) {
+        size_t bytes_read, bytes_to_read;
+
+        data = realloc( data, data_capacity );
+
+        if (data == nullptr) {
+            const char *error_message = "realloc: memory allocation failed\n";
+            write(2, error_message, strlen(error_message));
+
+            return (struct blob_type) {};
+        }
+
+        //calculate number of bytes to read in next read operation
+        bytes_to_read = data_capacity - data_size;
+
+        //attempt to fill the read buffer
+        bytes_read = fread( data + data_size, 1, bytes_to_read, fp );
+
+        //update size of data
+        data_size += bytes_read;
+
+        //break out of the infinite loop if the read buffer could
+        //not be filled entirely
+        if (bytes_read != bytes_to_read) {
+            break;
+        }
+
+        //change desired capacity for next loop iteration
+        data_capacity *= 2;
+    }
+
+    fclose(fp);
+
+    return (struct blob_type) { .data = data, .size = data_size };
+}
